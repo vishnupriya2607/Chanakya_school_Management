185f154c5506b0e78c3d20c7ced1809d501152f6
vishnupriya2607 <125855591+vishnupriya2607@users.noreply.github.com>
2025-03-10 14:09:35 +0530

    changes

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'staff'], required: true }, // Role-based access
-});
-
-export default mongoose.model('User', UserSchema);
